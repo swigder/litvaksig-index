25bd3ea8a4fb60f550f29ade9726c8e65a320c69
suri <swigder@gmail.com>
2019-02-20 23:03:40 -0500

    Person toString does not print nulls.
    Easier to read.
    
    Excludes givenName, which should never be null.

--- src/main/java/litvaksig/index/data/Person.java
+++ src/main/java/litvaksig/index/data/Person.java
@@ -3,13 +3,11 @@ package litvaksig.index.data;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.ToString;
 
 import java.time.LocalDate;
 
 @Getter
 @Builder
-@ToString
 @EqualsAndHashCode
 public class Person {
     private String givenName;
@@ -21,4 +19,17 @@ public class Person {
 
     private Person father;
     private Person mother;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "givenName='" + givenName + '\'' +
+                (surname != null ? ", surname='" + surname + '\'' : "") +
+                (birthSurname != null ? ", birthSurname='" + birthSurname + '\'' : "") +
+                (birthDate != null ? ", birthDate='" + birthDate + '\'' : "") +
+                (birthPlace != null ? ", birthPlace='" + birthPlace + '\'' : "") +
+                (father != null ? ", father='" + father + '\'' : "") +
+                (mother != null ? ", mother='" + mother + '\'' : "") +
+                '}';
+    }
 }
